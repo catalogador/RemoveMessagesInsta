27bbb7c9d84ff8b6dca6c6f978079da8d2b57eb1
weslley francisco <90652376+catalogador@users.noreply.github.com>
2025-12-04 13:42:57 -0300

    Add files via upload

---
+++ removeMessages/content.js
@@ -0,0 +1,11 @@
+function removerDivEspecifica() {
+    const alvo = document.querySelector(
+        '.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1.x3h4tne.x145d82y.xixxii4'
+    );
+
+    if (alvo) {
+        alvo.remove();
+    }
+}
+
+setInterval(removerDivEspecifica, 200);
